8327a9484f3a1e710f93e48612897c936ad44595
Paradox021 <espanacarmelo@gmail.com>
2023-05-26 09:10:57 +0200

    añadiendo router de market

---
+++ routers/marketRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import upload from '../libs/storage.js'
+import * as marketController from '../controllers/marketController.js'
+
+const marketRouter = Router()
+
+marketRouter.get('/:marketId/offers', marketController.getMarketOffers)
+marketRouter.post('/:marketId/offers', marketController.addOffer)
+marketRouter.post('/:marketId/offers/:offerId/buy', marketController.buyOffer)
+marketRouter.delete('/:marketId/offers/:offerId', marketController.removeOffer)
