f7be2fd68b6c7e932a31a9cf40391829fdab62be
Paradox021 <espanacarmelo@gmail.com>
2024-03-17 23:37:05 +0100

    redirigir a discordAuth

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ import authService from '../services/authService.js';
 const discordAuth = async (req, res) => {
     try {
         const discordAuthUrl = process.env.DISCORD_AUTH_URL;
+        res.redirect(discordAuthUrl);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
