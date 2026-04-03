95139751c06173c0da526d01008358cc2dd38042
Paradox021 <espanacarmelo@gmail.com>
2026-04-03 02:52:32 +0200

    minor fix reordenar rutas user

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,12 +10,14 @@ userRouter.post('/id', userController.getUser)
 userRouter.post('/', userController.createUser)
 userRouter.delete('/:id', userController.deleteUser)
 userRouter.post('/:id/dailyBalance', userController.dailyBalance)
-userRouter.post('/:id/card/:cardId', userController.addCard)
+
 userRouter.delete('/:id/card/:cardId', userController.removeCard)
 userRouter.post('/:id/balance/:amount', userController.addBalance)
 userRouter.delete('/:id/balance/:amount', userController.removeBalance)
 userRouter.get('/:id/cards', userController.getUserWithCards)
 userRouter.get('/:id/cards/number', userController.getUserWithNumberOfCards)
 userRouter.post('/:id/card/random', userController.rollRandomCard)
+
+userRouter.post('/:id/card/:cardId', userController.addCard)
 
 export default userRouter
